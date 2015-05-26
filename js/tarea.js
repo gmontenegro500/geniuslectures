6f3375da43445970c22c9e4519eadac34f5e4873
gmontenegro500 <GDeveloper@Gs-Mac-mini.local>
2015-05-26 18:31:31 -0400

    fibpnacci
    fibpnacci

--- js/tarea.js
+++ js/tarea.js
@@ -2,23 +2,24 @@
 // for es un ciclo para cuando sabes exactamente cuantas vueltas tienes que dar
 //	while es un ciclo para cuando no lo sabes
 
+$('#clicked-button-4').click(function(){
+	$('#wrap-4').removeClass('contenido').addClass('contenido-top');
 
-function fibonacci(){
+	var x = $('#entrada-4').val();
 	var x1 = 1;
 	var x2 = 1;
 	var aux;
 	
-		console.debug(x1);
-		console.debug(x2);
 	
-	while(x2 < 100){	
+	while(x2 <= x){	
 		aux = x2;					
 		x2 = x2 +x1;				
 		x1 = aux;					
 
-		console.debug(x2);
+		$('#respuesta-4').html("Fibonacci: 1, 1, "+x2+", ").show(1000);
 	}
-}
+
+});
 
 function es_primo(x){
 
@@ -58,24 +59,15 @@ $('#clicked-button-2').click(function () {
 	$('#wrap-2').removeClass('contenido').addClass('contenido-top');
 
 	var x = $('#entrada-2').val();
-	var i;
 	var lista = 0;
 	
 	
-	for( i = 1; i <= x; i++){
+	for( var i = 1; i <= x; i++){
 		
-		// a continuacion verificamos si i es primo
-		var j;
-		var cont = 0;
-		
-		for(j = 1; j <= i; j++){
-			if(i % j == 0){
-				cont = cont + 1;
-			}
-		}
-		if(cont == 2){
+		if(es_primo(i)){
 			lista = lista + 1;
-		}	
+		}
+	
 	} 
 	$('#respuesta-2').html("Existen : " + lista +" numeros primos menores o iguales que su numero ---> " +x).show('fast');
 
@@ -87,38 +79,19 @@ $('#clicked-button-3').click(function () {
 	$('#wrap-3').removeClass('contenido').addClass('contenido-top');
 
 	var x = $('#entrada-3').val();
-	var i;
 	var lista = 0;
 	
 	
-	for( i = 1; i <= x; i++){
+	for( var i = 1; i <= x; i++){
 		
-		// a continuacion verificamos si i es primo
-		var j;
-		var cont = 0;
-		for(j = 1; j <= i; j++){
-			if(i % j == 0){
-				cont = cont + 1;
-			}
-		}
-		if(cont == 2){
+		if(es_primo(i)){
 			lista = lista + 1;
-		}	
-	}
-	//final
-	
-	var k;
-	var cuenta = 0;
-
-	for(k=1; k <= lista; k++){
-		if(lista % k == 0){
-			cuenta = cuenta + 1;
 		}
 	}
-	if(cuenta == 2){	
-		$('#respuesta-3').html("Existen : " + lista +" numeros primos menores o iguales que su numero ---> " +x+ "<p>El numero: "+lista+ " ES un numero primo<p>").show('fast');
-	} else{
-		$('#respuesta-3').html("Existen : " + lista +" numeros primos menores o iguales que su numero ---> " +x+ "<p>  El numero: "+lista+ " NO es un numero primo</p>").show('fast');
-	}
+		if(es_primo(lista)){
+			$('#respuesta-3').html("Existen : " + lista +" numeros primos menores o iguales que su numero ---> " +x+ "<p>El numero: "+lista+ " ES un numero primo<p>").show('fast');
+		}else{
+			$('#respuesta-3').html("Existen : " + lista +" numeros primos menores o iguales que su numero ---> " +x+ "<p>  El numero: "+lista+ " NO es un numero primo</p>").show('fast');
+		}
 
 });
